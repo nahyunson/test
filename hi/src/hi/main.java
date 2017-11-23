19083db183be47fa65553550972b85f02d68c291
nahyunson <nhson@rnd.skinfosec.com>
2017-11-23 17:40:01 +0900

    oo

--- hi/src/hi/main.java
+++ hi/src/hi/main.java
@@ -4,7 +4,7 @@ public class main {
 	public main() {
 		System.out.println("hi!!");
 		System.out.println("hi11ddd111!!");
-		System.out.println("hi122ddd22!!");
+		System.out.println("hi12222!!");
 	}
 
 }
