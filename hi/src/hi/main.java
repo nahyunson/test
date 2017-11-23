65102553279667091383516b08e6a234fbe3739a
nahyunson <nhson@rnd.skinfosec.com>
2017-11-23 17:09:59 +0900

    qqq

--- hi/src/hi/main.java
+++ hi/src/hi/main.java
@@ -3,8 +3,8 @@ package hi;
 public class main {
 	public main() {
 		System.out.println("hi!!");
-		System.out.println("hi11111!!");
-		System.out.println("hi12222!!");
+		System.out.println("hi11ddd111!!");
+		System.out.println("hi122ddd22!!");
 	}
 
 }
